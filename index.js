c606ceb5ea663192d6c9008bb30fedc1ac2de14e
Andita Kaesar <anditakaesar@gmail.com>
2020-02-01 15:43:21 +0700

    refactor server io

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const moment = require('moment');
-
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static('public'));
-
-io.on('connect', (socket) => {
-    console.log('a client connected');
-    socket.on('disconnect', function(){
-        console.log('a client disconnected');
-    });
-});
-
-app.get('/', (req, res, next) => {
-    res.render('index.ejs');
-});
-
-app.post('/', (req, res, next) => {
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    
-    let data = {
-        date: moment().format('YYYY-MM-DD HH:mm:ss.SSS'),
-        ip: ip,
-        data: req.body
-    }
-
-    io.emit('body_result', data);
-    res.sendStatus(200);
-});
+const http = require('./app').http;
 
 http.listen(3002, () => {
     console.log(`App listen at port 3002`);
